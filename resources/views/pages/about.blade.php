30bb073f48aefd8b0c615a9584a412eb6c1cc1ab
jhon-swai <jhonswai@gmail.com>
2019-10-24 19:37:46 +0300

    Added about and index pages

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@section('title')
+Laravel 6.4.0 Basics | About page
+@endsection
+@section('content')
+    <h3>About Page</h3>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+@endsection
